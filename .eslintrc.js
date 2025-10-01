beffbafa347492a80d154fcd96614c478cc7245c
salmondarat <salmonddarat@gmail.com>
2025-10-02 02:38:29 +0700

    Initiate Projects

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["@monogym/eslint-config"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/"]
+    }
+  }
+};
---
+++ apps/docs/eslint.config.js
@@ -0,0 +1,12 @@
+import { nextJsConfig } from "@repo/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...nextJsConfig,
+  {
+    files: ["next-env.d.ts"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+];
